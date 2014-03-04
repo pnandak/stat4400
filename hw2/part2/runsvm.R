afc9e9f0f8decd8faa8652dd877ad8fcf0b50974
Tim Paine <t.paine154@gmail.com>
2014-03-04 05:01:39 -0500

    part 2 work done.

--- hw2/part2/runsvm.R
+++ hw2/part2/runsvm.R
@@ -4,42 +4,49 @@
 
 #runsvm("/Volumes/Macintosh HD/Users/theocean154/Documents/School_files/College/Semester 8/STAT 4400/HW/stat4400/hw2/part2/uspsdata.txt","/Volumes/Macintosh HD/Users/theocean154/Documents/School_files/College/Semester 8/STAT 4400/HW/stat4400/hw2/part2/uspscl.txt", "/Volumes/Macintosh HD/Users/theocean154/Documents/School_files/College/Semester 8/STAT 4400/HW/stat4400/hw2/part2/uspsdata_test.txt", "/Volumes/Macintosh HD/Users/theocean154/Documents/School_files/College/Semester 8/STAT 4400/HW/stat4400/hw2/part2/uspscl_test.txt")
 
-runsvm <- function(data,class,data_test,class_test){
+
+runsvm <- function(data,class){
+    if(! require(e1071))
+    {
+        install.packages("e1071")
+    }
+
+    library(e1071)
+    library(rpart)
+
 
     d <- read.table(data, header = FALSE, sep = "", skip = 0)
-    d_test <- read.table(data_test, header = FALSE, sep = "", skip = 0)
     c <- read.table(class, header = FALSE, skip = 0)
-    c_test <- read.table(class_test, header = FALSE, skip = 0)
 
     d <- as.matrix(d)
-    d_test  <- as.matrix(d_test)
-    c <- as.matrix(c)
-    c_test <- as.matrix(c_test)
-
-    x <- d
-    y <- c
-    model <- svm(data=x,x,y, cost=70, gamma = .003)
-    print(model)
-    summary(model)
-
-    pred <- predict(model, x)
-    print(table(pred, y))
-    plot(cmdscale(dist(d)),
-      col = as.integer(pred<0)+1,
-        pch = c("o","+")[1:150 %in% model$index + 1])
+    class <- as.matrix(c)
+    colnames(class) <- "class"
+    d <- data.frame(cbind(d,class))
 
 
-    pred <- predict(model, d_test, decision.values = TRUE)
-    print(table(pred,c_test))
-    attr(pred, "decision.values")
-    plot(cmdscale(dist(d_test)),
-      col = as.integer(pred<0)+1,
-        pch = c("o","+")[1:150 %in% model$index + 1])
+    f <- as.formula(paste(tail(names(d), 1), "~ ."))
 
-#    plot(cmdscale(dist(p[,-ncol(p)])),
- #       col = as.integer(p[,ncol(p)])+4,
-  #      pch = c("o","+")[1:150 %in% model$index + 1])
+    index <- 1:nrow(d)
+    testindex <- sample(index, trunc(length(index)/5))
+    testset <- d[testindex,]
+    trainset <- d[-testindex,]
 
 
 
+    svm.model <- svm(f, data=trainset, cost=70, gamma = 1, cross =5)
+    #model <- svm(data=x, x, y, cost=70, gamma = 0.003, cross=5)
+    print(summary(svm.model))
+
+    svm.pred <- predict(svm.model, testset[,-ncol(d)])
+
+    #print(summary(pred))
+    #print(attr(pred, "decision.values"))
+    #plot(cmdscale(dist(testset)),
+     # col = as.integer(pred<0)+1,
+      #  pch = c("o","+")[1:150 %in% model$index + 1])
+
+    tab <- table(pred = svm.pred, true = testset[,ncol(d)])
+
+    print(tab)
+    print(classAgreement(tab))
 }
